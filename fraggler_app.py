d401847d31b5857aa19548a957f786284987a28e
Thaddaeus Sandidge <thaddaeussandidge@gmail.com>
2024-11-04 12:58:58 -0800

    Removed Docker images for lighter merge and updated test with new logging format

--- fraggler_app.py
+++
@@ -1,6 +0,0 @@
-import fraggler.fraggler_gui as fg
-
-"""Entry point for the application script to run fraggler GUI from source. 
-    ELimnates import error in structure of PyPi package"""
-
-fg.run()
